db9de2b4a4745543e1a3e0af7b7bb74e8fddb2e7
Yurino Oga <yurino.o@latona.io>
2022-01-18 15:50:33 +0900

    release

---
+++ location-on-satellite-orbit-sql-header-data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `location_on_satellite_orbit_header_data`
+(
+  `Satellite`                     varchar(20) NOT NULL,
+  `OrbitLocationID`                    varchar(20) NOT NULL,
+  `OrbitLocationName`                  varchar(20) DEFAULT NULL,
+  `XCoordinateOfCenter`           varchar(20) DEFAULT NULL,
+  `YCoordinateOfCenter`           varchar(20) DEFAULT NULL,
+  `Address1`                      varchar(60) DEFAULT NULL,
+  `Address2`                      varchar(60) DEFAULT NULL,
+  `Address3`                      varchar(60) DEFAULT NULL,
+  `Comment`                       varchar(255) DEFAULT NULL,
+    PRIMARY KEY (`Satellite`, `LocationID`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
